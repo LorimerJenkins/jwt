0ef01c01be309c52910930f707756e8079b99243
Lorimer Jenkins <90644448+LorimerJenkins@users.noreply.github.com>
2022-11-04 20:28:22 +0000

    -

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -3,27 +3,20 @@ import jwtDecode from 'jwt-decode';
 import './App.css';
 import Arweave from 'arweave';
 import { dataTransaction } from './interactWithAR.js';
-import Records from './database.json'
 
 
 function App() {
-
   const [ user, setUser ] = useState({});
   const [ JWT, setJWT ] = useState({});
-
   function handleCallbackResponse(response) {
     var rawJWT = response.credential
     setJWT(rawJWT)
-
     var userObject = jwtDecode(response.credential)
     setUser(userObject);
-    
     document.getElementById('signInDiv').hidden = true;
-    // document.getElementById('signInText').hidden = true;
+    document.getElementById('signInText').hidden = true;
     document.getElementById('profile').style.display = 'flex'
   }
-
-
 
   function handleSignOut(event) {
     // clears var
@@ -32,82 +25,80 @@ function App() {
     document.getElementById('signInText').hidden = false;
     document.getElementById('profile').style.display = 'none'
   }
-
   useEffect(() => {
     /* global google */
     google.accounts.id.initialize({
       client_id: '452069452454-acvtmk3imjmbpgnrcti5k5vhgqe04t9v.apps.googleusercontent.com',
       callback: handleCallbackResponse
     });
-
     google.accounts.id.renderButton(
       document.getElementById('signInDiv'), 
       { theme: 'outline', size: 'large' }
     );
-
-
   }, [])
 
 
 
-  
   // connect to arweave
   const arweave = Arweave.init({});
 
 
+  function walletData(key, TF) {
 
-  async function key_check(email, key1) {
+    if (TF == 'yesParse') {
+      var key = JSON.parse(key)
+    } else {}
 
-    const url = 'https://lorimer0jwt.pythonanywhere.com/check_key/?email=' + email + '&&key=' + key1
+    // get wallet address private key
+    arweave.wallets.jwkToAddress(key).then((address) => {
+    document.getElementById('address').innerHTML = address
+
+    // get address balance
+    arweave.wallets.getBalance(address).then((balance) => {
+      let ar = arweave.ar.winstonToAr(balance);
+      document.getElementById('balance').innerHTML = ar
+    });
+  });
+}
+
+
+  const [ clientKey, setKey ] = useState({});
+  async function check_key(email) {
+    const url = 'https://lorimer0jwt.pythonanywhere.com/check_key/?email=' + email
     const response = await fetch(url);
     const response_json = await response.json();
-    const key2 = await response_json.response
+    const data = await response_json.response
 
-    console.log(key2)
-  
+
+    if (data == 'not found') {
+      // generate wallet
+      arweave.wallets.generate().then((key) => {
+
+        async function add_key(email, key) {
+          const key_send_format = JSON.stringify(key)
+          const url = 'https://lorimer0jwt.pythonanywhere.com/add_key/?email=' + email + '&&key=' + key_send_format
+          const response = await fetch(url);
+          const response_json = await response.json();
+          const data = await response_json.response
+        }
+        add_key(email, key)
+        walletData(key, 'noParse')
+        var parsedClientKey = JSON.parse(key)
+        setKey(parsedClientKey)
+      });
+
+    } else {
+      walletData(data, 'yesParse')
+      var parsedClientKey = JSON.parse(data)
+      setKey(parsedClientKey)
+    }
   }
 
-  // key_check(user.email, '12345')
 
-
-
-
-  // check if client has already a key
-  // const clientEmail = user.email
-  // if (clientEmail in Records) {
-  //   const key = Records[clientEmail]
-  // } else {
-  //   const key = Records['lorimer@wallety.org']
-  // }
-
-
-
-// generate wallet
-  // arweave.wallets.generate().then((key) => {
-  //   console.log(key)
-  // });
-
-
-
-  // temp key
-  const clientEmail = 'lorimer@wallety.org'
-  // const clientEmail = 'lorimerjenkins1@gmail.com'
-
-  const key = Records[clientEmail]
-
-  // get wallet address private key
-  const [ address, setAddress ] = useState({});
-  arweave.wallets.jwkToAddress(key).then((address) => {
-  setAddress(address)
-  document.getElementById('address').innerHTML = address
-});
-
-  // get address balance
-  arweave.wallets.getBalance(address).then((balance) => {
-  let ar = arweave.ar.winstonToAr(balance);
-  document.getElementById('balance').innerHTML = ar
-});
-
+  if (user.email != undefined) {
+     check_key(user.email)
+  }
+ 
 
 
 
@@ -115,7 +106,7 @@ function App() {
     <div className="app">
 
       <div className='signInDiv' id='signInDiv'></div>
-      {/* <h1 className='large-title' id='signInText'>Arweave JWT Authenticator</h1> */}
+      <h1 className='large-title' id='signInText'>Arweave JWT Authenticator</h1>
       <img className='favicon' src='favicon.png'></img>
 
 
@@ -140,7 +131,7 @@ function App() {
 
         <div className='saveTextDiv'>
           <input className='input' placeholder='Text to Permaweb' id='input' type="text/number/something-other-than-password" name="x-field-w" autoComplete="new-field-w"></input>
-          <button className='submit-TXN' onClick={() => dataTransaction(key)}>Submit Transaction</button>
+          <button className='submit-TXN' onClick={() => dataTransaction(clientKey)}>Submit Transaction</button>
         </div>
 
       </div>
--- server/flask_app.py
+++ server/flask_app.py
@@ -1,19 +1,7 @@
 import json
 import requests
 
-
-# email = 'lorimer@wallety.org'
-# key = '1234'
-
 filename = '/home/lorimer0jwt/mysite/database.json'
-
-def add_key(email, key):
-    with open(filename, "r") as file:
-        data = json.load(file)
-    data[email] = key
-    with open(filename, "w") as file:
-        json.dump(data, file)
-# add_key(email=email, key=key)
 
 
 def check_if_key(email):
@@ -22,21 +10,27 @@ def check_if_key(email):
     if email in data:
         return data[email]
     else:
-        return False
-# print(check_if_key('lorimer@wallety.org'))
+        return 'not found'
 
 
+def add_key(email, key):
+    if check_if_key(email) == 'not found':
+        with open(filename, "r") as file:
+            data = json.load(file)
+        data[email] = key
+        with open(filename, "w") as file:
+            json.dump(data, file)
+        return 'added'
+    else:
+        return 'added'
 
-def the_works(email, key):
 
+def search_key(email):
     key_check = check_if_key(email)
-
-    if key_check != False:
+    if key_check != 'not found':
         return key_check
     else:
-        add_key(email, key)
-        return key
-
+        return 'not found'
 
 
 
@@ -55,20 +49,34 @@ def home():
     return 'Working'
 # Check #############################################################################################################################################################
 @app.route('/check_key/', methods=['GET'])
-def check():
+def check_the_key():
     try:
         email = str(request.args.get('email'))
-        key = str(request.args.get('key'))
 
-        check = the_works(email, key)
-        check = json.loads(check)
+        check = search_key(email=email)
 
         return Response(dumps({'response': check}), mimetype='text/json')
+
     except Exception as theError:
         telegram_api_key = '5119024151:AAEZ0mSok1-2cuqelU26Ex0Nio04EuCDy4Q'
         telegram_chat_id_website_hits = '-1001515674184'
         requests.get(f'https://api.telegram.org/bot{telegram_api_key}/sendMessage?chat_id={telegram_chat_id_website_hits}&text={theError}')
-        return Response(dumps({'server_status': 400}), mimetype='text/json')
+        return Response(dumps({'response': 400}), mimetype='text/json')
+# Add key #############################################################################################################################################################
+@app.route('/add_key/', methods=['GET'])
+def add_the_key():
+    try:
+        email = str(request.args.get('email'))
+        key = str(request.args.get('key'))
+
+        adding_key = add_key(email=email, key=key)
+
+        return Response(dumps({'response': adding_key}), mimetype='text/json')
+    except Exception as theError:
+        telegram_api_key = '5119024151:AAEZ0mSok1-2cuqelU26Ex0Nio04EuCDy4Q'
+        telegram_chat_id_website_hits = '-1001515674184'
+        requests.get(f'https://api.telegram.org/bot{telegram_api_key}/sendMessage?chat_id={telegram_chat_id_website_hits}&text={theError}')
+        return Response(dumps({'response': 400}), mimetype='text/json')
 # RUN SERVER ##############################################################################################################################################################
 if __name__ == '__main__':
     app.run(port=7777)
